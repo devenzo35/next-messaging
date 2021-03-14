8523c1ddba60d0f68b0546cf490c583754e8e625
Enzo <enzocuellar12@gmail.com>
2021-03-14 01:22:12 -0300

    ui improves in sing in and sign up screens

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function register() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-indigo-700">
+      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg">
         <div>
           <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            className="mx-auto h-36 w-auto"
+            src="/undraw_Work_chat_re_qes4.svg"
             alt="Workflow"
           ></img>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+          <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
+            Start be connect with your friends
           </h2>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
@@ -102,10 +102,7 @@ export default function register() {
               <b style={{ position: "relative" }}>Google</b>
             </button>
             <div className="text-sm">
-              <Link
-                href="/auth/login"
-                /* className="font-medium text-indigo-600 hover:text-indigo-500" */
-              >
+              <Link href="/auth/login" className="hover:text-indigo-700">
                 Do you already have an account?
               </Link>
             </div>
@@ -116,21 +113,7 @@ export default function register() {
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
               Sign in
             </button>
           </div>
